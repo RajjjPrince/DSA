8e46d2de2c61d687c279aa7db93163c9116593ed
RajjjPrince <ece22144@iiitkalyani.ac.in>
2025-05-27 20:01:37 +0530

    Add a function display Doubly LinkedList.

--- doublyLinkedList.java
+++ doublyLinkedList.java
@@ -1,5 +1,70 @@
 package linkedList;
 
 public class doublyLinkedList {
-
+	private ListNode head;
+	private ListNode tail;
+	private int length;
+	private class ListNode{
+		
+		private int data;
+		private ListNode previous;
+		private ListNode next;
+		public ListNode(int data) {
+			this.data = data;		
+			}
+	}
+		
+		public doublyLinkedList(){
+			this.head = null;
+			this.tail = null;
+			this.length = 0;
+		}
+		
+		public boolean isEmpty() {
+			return length == 0;
+		}
+		public void insertLast(int value) {
+			ListNode newNode = new ListNode(value);
+			if(isEmpty()) {
+				head = newNode ;
+			}else{
+				tail.next = newNode;
+			}
+			newNode.previous = tail;
+			tail = newNode;
+			length++;
+		}
+		 public void displayForward() {
+			 ListNode curr = head;
+			 while(curr!= null) {
+				 System.out.print(curr.data +"-->");
+				 curr = curr.next;
+			 }
+			 System.out.print("null");
+			 System.out.println();
+		 }
+		
+		 public void displayBackward() {
+			 ListNode curr = tail;
+			 while(curr!= null) {
+				 System.out.print(curr.data +"-->");
+				 curr = curr.previous;
+			 }
+			 System.out.print("null");
+			 System.out.println();
+		 }
+		
+		
+		public static void main(String[] args) {
+			doublyLinkedList dll = new doublyLinkedList();
+			dll.insertLast(0);;
+			dll.insertLast(0);
+			dll.insertLast(0);
+			dll.insertLast(5);
+			
+			dll.displayForward();
+			dll.displayBackward();
+			
+		}
+	
 }
