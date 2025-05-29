44bc46bd2b0b45a26121e61c7ed622af1bc13f74
RajjjPrince <ece22144@iiitkalyani.ac.in>
2025-05-29 20:47:53 +0530

    Added a method to remove extra spaces in reverse string.

--- src/recursion/reverseString.java
+++ src/recursion/reverseString.java
@@ -4,13 +4,17 @@ public class reverseString{
 		if(input.equals("")) {
 			return "";
 		}
+		
 		return revString (input.substring(1))+input.charAt(0);
 	}
 	
-	
+	public static String cleanSpace(String input) {
+		return input.trim().replaceAll("\\s+"," ");
+	}
 public static void main (String [] args) {
-	String input = "hello world   ";
+	String input = "   hello    world   ";
 	System.out.println("Given String input : "+input);
-	System.out.println("Revese String : "+revString(input));
+	input = cleanSpace(input);
+	System.out.println("Revese String :"+revString(input));
 	}
 }
